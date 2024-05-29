ece21b0c9bf78dcceae6a0cf9b5a90b3794acc0a
TerZer <robertas.pvlovskis171@gmail.com>
2024-05-29 14:23:52 +0300

    Fixed stream reopen

--- libuvccamera/src/main/jni/libuvc/src/stream.c
+++ libuvccamera/src/main/jni/libuvc/src/stream.c
@@ -726,7 +726,7 @@ static void _uvc_process_payload(uvc_stream_handle_t *strmh, const uint8_t *payl
 		if (UNLIKELY(header_info & UVC_STREAM_ERR)) {
 //			strmh->bfh_err |= UVC_STREAM_ERR;
 			UVC_DEBUG("bad packet: error bit set");
-			libusb_clear_halt(strmh->devh->usb_devh, strmh->stream_if->bEndpointAddress);
+			//libusb_clear_halt(strmh->devh->usb_devh, strmh->stream_if->bEndpointAddress);
 //			uvc_vc_get_error_code(strmh->devh, &vc_error_code, UVC_GET_CUR);
 			uvc_vs_get_error_code(strmh->devh, &vs_error_code, UVC_GET_CUR);
 //			return;
